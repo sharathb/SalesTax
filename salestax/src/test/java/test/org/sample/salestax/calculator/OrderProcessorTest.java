40f3b53f29e77a6755fedefc0754f47b1ac3c666
Sharath.Bhaskara <sbhaskara@paypal.com>
2011-11-04 17:45:49 +0530

    Import to Git Repo

---
+++ salestax/src/test/java/test/org/sample/salestax/calculator/OrderProcessorTest.java
@@ -0,0 +1,26 @@
+package test.org.sample.salestax.calculator;
+
+import java.nio.file.FileSystems;
+import java.util.List;
+
+import org.junit.Test;
+import org.sample.salestax.OrderProcessor;
+import org.sample.salestax.product.IOrder;
+import org.sample.salestax.product.Order;
+
+public class OrderProcessorTest {
+
+	@Test
+	public void testOrderPrcoessor() {
+		Order[] orders;
+		List<IOrder> processedOrders;
+		OrderProcessor processor;
+		processor = new OrderProcessor();
+		orders = processor.createOrdersFromFiles(new String[] {
+				FileSystems.getDefault().getPath("Orders").toString(),
+				"Order1.txt", "Order2.txt", "Order3.txt" });
+		processedOrders = processor.processOrders(orders);
+		processor.generateInvoice(processedOrders);
+	}
+
+}
